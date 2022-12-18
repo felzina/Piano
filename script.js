c8fe131c1796181f3e897c2c68116eba0cc32647
Liza Felzina <felzina_liza@mail.ru>
2022-12-18 23:26:46 +0300

    feat: keys with keyboard

--- script.js
+++ script.js
@@ -5,9 +5,18 @@ let audio = new Audio("tunes/a.wav");
 const playTune = (key) => {
     audio.src = `tunes/${key}.wav`;//passing audio scr based on key pressed
     audio.play(); //playing audio
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`); //getting clicked key element
+    clickedKey.classList.add("active");
 };
 
 pianoKeys.forEach(key => {
     //calling playTune function with data-key as an argument
     key.addEventListener("click", () => playTune(key.dataset.key));
 });
+
+const pressedKey = (e) => {
+    playTune(e.key);
+}
+
+document.addEventListener("keydown", pressedKey);
