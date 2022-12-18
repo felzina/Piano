ff20a76365eb66ec02c42882b2e24aec8bb135ec
Liza Felzina <felzina_liza@mail.ru>
2022-12-18 22:55:45 +0300

    feat: some changes

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const pianoKeys = document.querySelectorAll('.piano-leys .key');
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
 let audio = new Audio("tunes/a.wav");
 
 const playTune = (key) => {
+    audio.src = `tunes/${key}.wav`;//passing audio scr based on key pressed
     audio.play(); //playing audio
 }
 
 pianoKeys.forEach(key => {
     //calling playTune function with data-key as an argument
-    key.addEventListener('click', () => playTune(key.dataset.key));
+    key.addEventListener("click", () => playTune(key.dataset.key));
 });
